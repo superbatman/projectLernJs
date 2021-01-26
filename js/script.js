386aedf6c835663b387da1e44bb921353ee58ee9
Zi <example@gmail.com>
2021-01-26 22:23:50 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let number = 5;
+let _number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a:50
+}
+obj.a = 10;
+console.log(obj);
+console.log(name);
+var name = 'ivan';
